7d5fd114e869cd09d886500f05d23626eb75cf39
mk <qds316@hotmail.com>
2022-08-08 10:03:19 +0800

    Use the atomic package

---
+++ www-atomic.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "runtime"
+  "sync/atomic"
+)
+
+var count int32
+
+func handleAll(w http.ResponseWriter, r *http.Request) {
+  atomic.AddInt32(&count, 1)
+}
+
+func getCounter(w http.ResponseWriter, r *http.Request) {
+  tmp := atomic.LoadInt32(&count)
+  fmt.Println("Count:", tmp)
+  fmt.Fprintf(w, "<h1 align=\"center\">%d</h1>", count)
+}
+
+func main() {
+  runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+  http.HandleFunc("/getCounter", getCounter)
+  http.HandleFunc("/", handleAll)
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+}
